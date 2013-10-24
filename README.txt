c554517145f0130e7e59a231fd36f873b2c48ddd
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2013-10-24 13:15:25 -0400

    Add other encoding options

--- README.txt
+++ README.txt
@@ -2,4 +2,11 @@ Requires zint (http://zint.github.io/).
 
 To run:
 
-python encode.py "string to encode" > result.sgf
+python encode.py [FORMAT] STRING
+  Where FORMAT is one of:
+     --qrcode
+     --datamatrix
+     --microqr
+     --grid
+     --aztec
+  Default: --qrcode
--- encode.py
+++ encode.py
@@ -1,28 +1,51 @@
 import sys
 import subprocess
 
-if __name__ == '__main__':
-    data = sys.argv[1]
-    encoded = subprocess.check_output('zint --dump -b 92 -d'.split(' ') + [data])
-    encoded = encoded.translate(None, "[] ").split('\n')
+formatargs = {'datamatrix': '--square -b 71',
+              'qrcode': '-b 58',
+              'grid': '-b 142',
+              'microqr': '-b 97',
+              'aztec': '-b 92'}
 
-    boardsize = max(len(l) for l in encoded)
+if __name__ == '__main__':
+    zintargs = formatargs['qrcode']
+
+    if '--help' in sys.argv[1:]:
+        print 'python encode.py [FORMAT] STRING'
+        print '  Where FORMAT is one of:'
+        for k in formatargs.keys():
+            print '     --{}'.format(k)
+        print '  Default: --qrcode'
+        sys.exit(0)
+
+    for arg in sys.argv[1:-1]:
+        if arg.startswith('-'):
+            arg = arg.translate(None, '-')
+            if arg in formatargs:
+                zintargs = formatargs[arg]
+            else:
+                print "No such format known: {}".format(arg)
+                sys.exit(1)
+
+    data = sys.argv[-1]
+    encoded = subprocess.check_output('zint --dump {} -d'.format(zintargs).split(' ') + [data])
+    encoded = [l for l in encoded.translate(None, "[] ").split('\n') if l]
+
+    boardsize = len(encoded)
 
     # header
     print "(;FF[4]SZ[{}]AP[gobancode]".format(boardsize)
 
-    row = boardsize
     black_stones = []
     white_stones = []
-    for l in encoded:
+    for row, l in enumerate(encoded):
         if l == '': continue
         for col, c in enumerate(l):
             dest = black_stones if c == '1' else white_stones
 
-            char_row = chr(ord('a') + row - 1)
+            char_row = chr(ord('a') + row)
             char_col = chr(ord('a') + col)
-            dest.append("[{}{}]".format(char_row, char_col))
-        row -= 1
+            dest.append("[{}{}]".format(char_col, char_row))
     print ";AB{}".format(''.join(black_stones))
     print ";AW{}".format(''.join(white_stones))
     print ")"
